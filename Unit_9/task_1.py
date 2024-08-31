2ead0a4e8f3addcc9685175a50a4a36c807eaaed
RShoose <tabulkovy@gmail.com>
2024-09-01 00:24:29 +0300

    Unit_9_done

---
+++ Unit_9/task_1.py
@@ -0,0 +1,11 @@
+"""Перетасовка элементов массива
+Цель: Написать функцию для случайной перетасовки элементов массива."""
+
+import numpy as np
+def shuffle_array(array):
+    np.random.shuffle(array)
+    return array
+
+# Пример использования
+arr = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+print(shuffle_array(arr))
