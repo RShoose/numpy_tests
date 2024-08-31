c9d7d02eeccf03b17487f34a05db8459f0931e8d
RShoose <tabulkovy@gmail.com>
2024-08-31 14:13:59 +0300

    Unit_7_done

---
+++ Unit_7/Task_5_add.py
@@ -0,0 +1,23 @@
+"""Фильтрация элементов в трехмерном массиве
+Цель: Построить функцию, которая принимает трехмерный массив 
+и возвращает двумерный массив, который содержит
+только те элементы каждого подмассива второго уровня,
+которые больше среднего значения всех элементов в соответствующем подмассиве."""
+
+import numpy as np
+
+def filter_elements_3d(arr):
+    result = []
+    for matrix in arr:
+        local_result = []
+        for subarray in matrix:
+            mean_value = sum(subarray) / len(subarray)  # Вычисляем среднее значение
+            # Формируем список элементов больше или равных среднему
+            filtered_elements = [x for x in subarray if x >= mean_value]
+            local_result.append(filtered_elements)
+        result.append(local_result)
+    return result
+
+# Пример входных данных и вызов функции
+example_3d_array = [[[1, 2, 3], [4, 5, 6]], [[7, 8, 9, 10], [10, 11, 12, 13]]]
+print(filter_elements_3d(example_3d_array))
